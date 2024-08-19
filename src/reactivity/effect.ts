bdd07857d2a9d2e62a7ed25eaaa6b4c410d94575
wangy075 <kevinwangyue@126.com>
2024-08-19 15:11:12 +0800

    feat: 改造runner

--- src/reactivity/effect.ts
+++ src/reactivity/effect.ts
@@ -1,39 +1,51 @@
 class ReactiveEffect {
-  private _fn;
-  constructor(fn) {
-    this._fn = fn;
+    private _fn: any;
+  
+    constructor(fn) {
+      this._fn = fn;
+    }
+    run() {
+      activeEffect = this;
+      return this._fn();
+    }
   }
-  run() {
-    this._fn();
-  }
-}
-let activeEffect;
-const targetMap = new Map();
-export function track(target, key) {
-  // target -> key -> dep
-  let depsMap = targetMap.get(target);
-  if (!depsMap) {
-    depsMap = new Map();
-    targetMap.set(target, depsMap);
-  }
-  let dep = depsMap.get(key);
-  if (!dep) {
-    dep = new Set();
-    depsMap.set(key, dep);
-  }
+  
+  const targetMap = new Map();
+  export function track(target, key) {
+    // target -> key -> dep
+    let depsMap = targetMap.get(target);
+    if (!depsMap) {
+      depsMap = new Map();
+      targetMap.set(target, depsMap);
+    }
 
-  dep.add(activeEffect);
-}
-export function trigger(target, key) {
-  let depsMap = targetMap.get(target);
-  let dep = depsMap.get(key);
-
-  for (const effect of dep) {
-    effect.run();
+  
+    let dep = depsMap.get(key);
+    if (!dep) {
+      dep = new Set();
+      depsMap.set(key, dep);
+    }
+  
+    dep.add(activeEffect);
   }
-}
-export function effect(fn) {
-  //fn
-  const _effect = new ReactiveEffect(fn);
-  _effect.run();
-}
+  
+  export function trigger(target, key) {
+    let depsMap = targetMap.get(target);
+    let dep = depsMap.get(key);
+  
+    for (const effect of dep) {
+      effect.run();
+    }
+  }
+//   全局effect函数的实例对象
+  let activeEffect;
+  export function effect(fn) {
+    // fn
+    const _effect = new ReactiveEffect(fn);
+  
+    _effect.run();
+    // 当调用 runner 的时候可以重新执行 effect.run
+    const runner = _effect.run.bind(_effect);
+    return runner
+  }
+  
--- src/reactivity/tests/effect.spec.ts
+++ src/reactivity/tests/effect.spec.ts
@@ -6,12 +6,32 @@ describe("effect", () => {
       age: 10,
     });
     let nextAge;
+    // 外部影响
     effect(() => {
       nextAge = user.age + 1;
     });
     expect(nextAge).toBe(11);
-    // updata
+    // updata，set
     user.age++;
     expect(nextAge).toBe(12);
+    expect(user.age).toBe(11);
   });
+  it("should return runner when call effect", () => {
+    // 当调用 runner 的时候可以重新执行 effect.run
+    // runner 的返回值就是用户给的 fn 的返回值
+    let foo = 0;
+    const runner = effect(() => {
+      foo++;
+      return foo;
+    });
+
+    expect(foo).toBe(1);
+    runner();
+    expect(foo).toBe(2);
+    expect(runner()).toBe(3);
+  });
+  // 第二个参数
+  // effect第一次执行的时候，会调用 fn
+  // set 不会执行fn而是执行scheduler
+  // 当执runner的时候，会执行fn
 });
