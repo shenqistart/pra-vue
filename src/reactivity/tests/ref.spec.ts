722fb90b26ffc64974f35fc83be3d2847def0b07
wangy075 <kevinwangyue@126.com>
2024-08-21 17:46:06 +0800

    feat: proxyRef

--- src/reactivity/tests/ref.spec.ts
+++ src/reactivity/tests/ref.spec.ts
@@ -1,5 +1,5 @@
 import { effect } from "../effect";
-import {  isRef, ref, unRef } from "../ref";
+import { isRef, ref, unRef, proxyRefs } from "../ref";
 import { reactive } from "../reactive";
 describe("ref", () => {
   it("happy path", () => {
@@ -36,7 +36,7 @@ describe("ref", () => {
     let dummy;
     effect(() => {
       dummy = a.value.count;
-    }); 
+    });
     expect(dummy).toBe(1);
     // set 值
     a.value.count = 2;
@@ -53,10 +53,34 @@ describe("ref", () => {
     expect(isRef(1)).toBe(false);
     expect(isRef(user)).toBe(false);
   });
-// 如果是ref 就获取到值，否则就返回对应参数
+  // 如果是ref 就获取到值，否则就返回对应参数
   it("unRef", () => {
     const a = ref(1);
     expect(unRef(a)).toBe(1);
     expect(unRef(2)).toBe(2);
+  });
+  it("proxyRefs", () => {
+    const user = {
+      age: ref(10),
+      name: "xiaohong",
+    };
+
+    // get 如果是ref,那么就返回.value的值
+    // 如果不是ref,那么就返回get的值
+    const proxyUser = proxyRefs(user);
+    // 如下两种的区别，不用在写value了。主要用在 template 这样的写法上
+    expect(user.age.value).toBe(10);
+    expect(proxyUser.age).toBe(10);
+    expect(proxyUser.name).toBe("xiaohong");
+
+    // 当给普通值的时候
+    proxyUser.age = 20;
+
+    expect(proxyUser.age).toBe(20);
+    expect(user.age.value).toBe(20);
+    // 当给ref的值
+    proxyUser.age = ref(10);
+    expect(proxyUser.age).toBe(10);
+    expect(user.age.value).toBe(10);
   });
 });
