7b1fad655c26feb4f639b1c6969589200e247276
wangy075 <kevinwangyue@126.com>
2024-08-23 15:57:59 +0800

    feat : slots 实现

--- src/runtime-core/component.ts
+++ src/runtime-core/component.ts
@@ -2,12 +2,14 @@ import { shallowReadonly } from "../reactivity/reactive";
 import { initProps } from "./componentProps";
 import { PublicInstanceProxyHandlers } from "./componentPublicInstance";
 import { emit } from "./componentEmit";
+import { initSlots } from "./componentSlots";
 export function createComponentInstance(vnode) {
   const component = {
     vnode,
     type: vnode.type,
     setupState: {},
     props: {},
+    slots: {},
     emit: () => {},
   };
   // 使用优化技巧 emit('add') 直接写事件，第一个参数完成，用户只能修改之后的参数
@@ -18,8 +20,7 @@ export function createComponentInstance(vnode) {
 export function setupComponent(instance) {
   // 将虚拟节点的 props 转到实例上面
   initProps(instance, instance.vnode.props);
-   // TODO
-  // initSlots()
+  initSlots(instance, instance.vnode.children);
   // 处理有状态的组件
   setupStatefulComponent(instance);
 }
