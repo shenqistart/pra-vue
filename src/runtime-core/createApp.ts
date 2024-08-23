8ab7a3f2992aad3241014e4eb07fd77f92b6fbf0
wangy075 <kevinwangyue@126.com>
2024-08-24 07:56:27 +0800

    feat: customRender

--- src/runtime-core/createApp.ts
+++ src/runtime-core/createApp.ts
@@ -1,14 +1,13 @@
-import { render } from "./renderer";
 import { createVNode } from "./vnode";
 
-export function createApp(rootComponent) {
-  return {
-    mount(rootContainer) {
-    // 先转换为虚拟节点
-    // 再从虚拟节点 vnode 做处理
-      const vnode = createVNode(rootComponent);
-      // 就是做 patch 功能
-      render(vnode, rootContainer);
-    },
+export function createAppAPI(render) {
+  return function createApp(rootComponent) {
+    return {
+      mount(rootContainer) {
+        const vnode = createVNode(rootComponent);
+
+        render(vnode, rootContainer);
+      },
+    };
   };
 }
